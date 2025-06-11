6107f847c9edc349abb8d0811a6bc35ebe545207
Ben Johnstone <benji.forrest@gmail.com>
2025-06-10 21:46:25 -0400

    updated button push to talk

--- client/src/ui-components/StartCard.js
+++ client/src/ui-components/StartCard.js
@@ -1,43 +1,65 @@
-import { Stack, Card, Heading, Paragraph, Button, Tooltip } from '@twilio-paste/core';
+import {
+  Stack,
+  Card,
+  Heading,
+  Paragraph,
+  Button,
+  // Tooltip,
+} from "@twilio-paste/core";
+
+import { useState } from "react";
+
 import { StopIcon } from "@twilio-paste/icons/esm/StopIcon";
+import { CallIcon } from "@twilio-paste/icons/esm/CallIcon";
 
 const StartCard = (props) => {
+  const [pausePressed, setPausePressed] = useState(true);
 
-    //  handler to start the ai conversation
-    const handleStartClick = () => {  
-      props.placeCall("browser-client")
-    }
+  //  handler to start the ai conversation
+  const handleStartClick = () => {
+    props.placeCall("browser-client");
+  };
 
-    //  handler to end the ai conversation
-    const handleStopClick = () => {  props.stopCall() }
+  //  handler to end the ai conversation
+  const handleStopClick = () => {
+    props.stopCall();
+  };
 
-    //  layout for the start card
-    let layout = (
-            <Stack orientation="vertical" spacing="space40">
+  //  layout for the start card
+  let layout = (
+    <Stack orientation="vertical" spacing="space40">
       <Card padding="space120">
         <Heading as="h2" variant="heading20">
           Conversation Relay WebRTC Quickstart
         </Heading>
         <Paragraph>
-          Connecting to an Ai Agent is simple. Press the button below to get started.
+          Connecting to an Ai Agent is simple. Press the button below to get
+          started.
         </Paragraph>
-        <Stack orientation={'horizontal'} spacing="space40">
-            <Tooltip text="Start a conversation" placement="bottom">
-              <Button variant="primary" onClick={handleStartClick}>
-                Talk to a ConversationRelay Agent
-              </Button>
-            </Tooltip>
-            <Tooltip text="Stop Ai'ing" placement="bottom">
-              <Button variant="destructive_icon" size={'reset'} onClick={handleStopClick}>
-                <StopIcon decorative={false} size="sizeIcon80" title="Stop" />
-              </Button>
-            </Tooltip>
+        <Stack orientation={"horizontal"} spacing="space40">
+          <Button
+            variant="secondary_icon"
+            pressed={pausePressed}
+            onClick={() => {
+              if (pausePressed) {
+                handleStartClick();
+              } else {
+                handleStopClick();
+              }
+              setPausePressed(!pausePressed);
+            }}
+          >
+            Talk to Conversation Relay Agent
+            {pausePressed ? (
+              <CallIcon decorative />
+            ) : (
+              <StopIcon decorative color="colorTextError" />
+            )}
+          </Button>
         </Stack>
       </Card>
     </Stack>
-    )
-    return layout
-}
+  );
+  return layout;
+};
 export default StartCard;
-
-
