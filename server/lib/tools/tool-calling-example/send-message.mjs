0338e69cc3abad27b412e966495bf2ebd499eb6a
Dan Bartlett <dbartlett@twilio.com>
2025-06-24 15:18:18 -0700

    removing cr-sessions.json from tracking, updating send-message

--- server/lib/tools/tool-calling-example/send-message.mjs
+++ server/lib/tools/tool-calling-example/send-message.mjs
@@ -14,35 +14,6 @@ export async function SendSMS(tool) {
   const randomQuote = await returnQuote();
 
   let messageObject = {
-    To: args.To,
-    MessageBody: randomQuote
-  };
-
-  await sendMessageWithTwilio(messageObject);
-
-  let toolResult = { message: "SMS with a quote has been sent!" };
-
-  await saveToolResult(tool, toolResult);
-
-  return true;
-
-}
-import { returnQuote } from "./tool-calling-example-resources.mjs";
-import { saveToolResult } from "../save-tool-result.mjs";
-import pkg from "../twilio-send-message.cjs";
-const { sendMessageWithTwilio } = pkg;
-
-export async function SendSMS(tool) {
-
-  console.info("in SendSMS and tool\n" + JSON.stringify(tool, null, 2));    
-  
-  let args = JSON.parse(tool.function.arguments);
-
-  console.info("args\n" + JSON.stringify(args, null, 2));  
-
-  const randomQuote = await returnQuote();
-
-  let messageObject = {
     To: args.to_phone_number,
     MessageBody: randomQuote
   };
